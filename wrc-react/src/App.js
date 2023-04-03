c6506ec9e67dfa6f67f61b11f0f6c2340dd1472d
Andy van der Raadt <andy@vanderraadt.codes>
2023-04-03 10:40:59 -0600

    added: hover state pointer

--- wrc-react/src/App.js
+++ wrc-react/src/App.js
@@ -13,26 +13,30 @@ function App() {
   return (
     <>
       <div className="app-body">
-        <section id="wrc" className="app-header pathway">
-          {/* <h1 className="pathway">
-            Sexual harassment is any unwelcome conduct, comment, gesture, or
-            contact that is gender-related or sexual in nature that makes the
-            recipient feel uncomfortable, unsafe, offended, or humiliated,
-            whether intended or not.
-          </h1> */}
+        <section id="wrc" className="app-header">
           <div>
             <div className="tool-container">
               <DecisionTreeTool />
             </div>
           </div>
+          <h1 className="pathway">
+            {/* Sexual harassment is any unwelcome conduct, comment, gesture, or
+            contact that is gender-related or sexual in nature that makes the
+            recipient feel uncomfortable, unsafe, offended, or humiliated,
+            whether intended or not. */}
+            We have created this tool for you to click through. It will help you
+            find the information you are looking for and can connect you to
+            someone who understands. If you aren’t ready to chat, you can find
+            websites to browse down below.
+          </h1>
         </section>
       </div>
       <div className="resources-tabs">
         <section id="resources" className="app-header ">
           <h4>Resources</h4>
           <h1 className="pathway">
-            Morbi tellus sem, tempor eget neque et, ullamcorper condimentum
-            ligula
+            Placeholder lorem ipsum morbi tellus sem, tempor eget neque et,
+            ullamcorper condimentum ligula.
           </h1>
           <div>
             <div className="tool-container">
